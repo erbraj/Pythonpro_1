119db92bd41a4f9fe3843a0a7843aade05ca0553
Brajendra <79088886+erbraj@users.noreply.github.com>
2022-12-07 18:03:31 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,7 +1,5 @@
 import os
 list1=[1,2,3,4,5]
-#list1.reverse()
+list1.reverse()
 #list2=(list1*2)*3
-for i in list1:
-  sum=i*2
-print("The reversed list is , print 2 times :",sum)
+print("The reversed list is :",+list)
