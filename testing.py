51201a32d2e134c1b41193c8d09bc14b0cefbb67
Brajendra <79088886+erbraj@users.noreply.github.com>
2022-12-12 14:22:58 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -2,8 +2,8 @@ import os
 #list1=[1,2,3,4,5,6,7,8,9,10]
 #list1.reverse()
 #print("The reversed list is :", list1)
-for i in range(2,10):
-  for j in range(2,10):
+for i in range(1,10):
+  for j in range(1,10):
     table=i*j
     print(table,end=" ")
   print()  
